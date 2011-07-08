97af70c12c5777c5be25beef6703d947944b2ef9
Martin Kneissl <m.kneissl@dasgip.de>
2011-07-08 15:44:25 +0200

    Instrumentation now works for devices via TCP.

--- src/main/scala/BaseAndroidTestProject.scala
+++ src/main/scala/BaseAndroidTestProject.scala
@@ -3,7 +3,7 @@ import sbt._
 abstract class BaseAndroidTestProject(info: ProjectInfo) extends BaseAndroidProject(info) with Installable {
 
   lazy val testEmulator = instrumentationTestAction(Emulator) describedAs("runs tests in emulator") dependsOn reinstallEmulator
-  lazy val testDevice = instrumentationTestAction(USBDevice) describedAs("runs tests on device") dependsOn reinstallDevice
+  lazy val testDevice = instrumentationTestAction(device) describedAs("runs tests on device") dependsOn reinstallDevice
 
   def instrumentationTestAction(executionTarget: ExecutionTarget) = adbTask(executionTarget, "shell am instrument -w "+manifestPackage+"/android.test.InstrumentationTestRunner") describedAs("runs instrumentation tests")
 }
